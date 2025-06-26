a1369eef8d93ee9d4e95ebbc7d6bf65be56c3c5c
Zack543210 <zack20222022@gmail.com>
2025-06-26 09:11:17 +0800

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -39,7 +39,7 @@ while True:
     surface.fill(pygame.Color('black'))
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             exit()
 
     #draw grid
